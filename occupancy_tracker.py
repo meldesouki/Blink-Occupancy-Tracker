d23371211a96208b9483a33c598d3128b0f1b692
meldesouki <43501322+meldesouki@users.noreply.github.com>
2021-12-22 15:26:30 -0500

    Update occupancy_tracker.py

--- occupancy_tracker.py
+++ occupancy_tracker.py
@@ -1,5 +1,13 @@
-import requests
+from selenium import webdriver
+import selenium
+from selenium.webdriver import Edge, EdgeOptions
+from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
+
+options = EdgeOptions()
+options.use_chromium = True
+
+driver = Edge(options = options)
 
 
 WOODSIDE_LOCATION_URL = 'ny/queens/56-02-roosevelt-avenue'
@@ -8,11 +16,15 @@ BASE_URL = 'https://locations.blinkfitness.com/'
 
 
 headers = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36 Edg/96.0.1054.62'
-req = requests.get(BASE_URL + WOODSIDE_LOCATION_URL, headers = headers)
-html_doc = req.content
-soup = BeautifulSoup(html_doc, 'html.parser')
+req = driver.get(BASE_URL + WOODSIDE_LOCATION_URL)
+# current_occupancy = driver.find_element_by_css_selector('div.Core-capacityStatus.js-capacity-status.Core-capacityStatus--green')
+# html_doc = req.content
+# soup = BeautifulSoup(req.page_source, 'html.parser')
 
 # print(soup)
-current_occupancy = soup.find('div', class_ = 'Core-capacityStatus js-capacity-status Core-capacityStatus--green')
+# current_occupancy = soup.find('div', class_ = 'Core-capacityStatus js-capacity-status Core-capacityStatus--green')
 
-print(current_occupancy.text)
+current_occupancy = driver.find_element(By.CSS_SELECTOR, 'div.Core-capacityStatus.js-capacity-status.Core-capacityStatus--green')
+f = open('current_occupancy.txt', 'a')
+f.write('\n' + current_occupancy.text)
+f.close()
