6391e0665bec3a38136fef01920c8bbeac743be4
meldesouki <43501322+meldesouki@users.noreply.github.com>
2022-01-29 14:25:39 -0500

    Updated clock.py

--- clock.py
+++ clock.py
@@ -3,7 +3,11 @@ from occupancy_tracker import cronjob
 
 scheduler = BlockingScheduler()
 
-scheduler.add_job(cronjob, 'cron', id = 'main_job', hour = '7-19', minute = 3, timezone= 'America/New_York') #0 , jitter = 300)
+# scheduler.add_job(cronjob, 'cron', id = 'main_job', hour = '7-19', minute = 3, timezone= 'America/New_York') #0 , jitter = 300)
+
+@scheduler.scheduled_job('cron', hour = '7-19', minute = 3, timezone= 'America/New_York')
+def chronological_job():
+    cronjob()
 
 @scheduler.scheduled_job('interval', minutes=2)
 def timed_job():
