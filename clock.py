8c4dbbb25d42a8049bc21f25cc049b89a3033d69
meldesouki <43501322+meldesouki@users.noreply.github.com>
2022-01-29 01:08:11 -0500

    Updated clock.py

--- clock.py
+++ clock.py
@@ -4,7 +4,7 @@ from occupancy_tracker import get_current_occupancy
 sched = BlockingScheduler()
 
 
-@sched.scheduled_job(('cron', day_of_week= 'mon-sun', hour = '7-19', minute = '0-59/30', timezone = 'America/New_York'))
+@sched.scheduled_job('cron', hour = '7-19', minute = '0-59/30', timezone = 'America/New_York')
 def scheduled_job():
     get_current_occupancy('Woodside')
     get_current_occupancy('Jackson Heights')
