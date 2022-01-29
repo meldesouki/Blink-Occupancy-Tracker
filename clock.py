e12e7e44c76877fb96f8073283b3c464cd075389
meldesouki <43501322+meldesouki@users.noreply.github.com>
2022-01-29 14:44:17 -0500

    Update clock.py

--- clock.py
+++ clock.py
@@ -5,7 +5,7 @@ scheduler = BlockingScheduler()
 
 # scheduler.add_job(cronjob, 'cron', id = 'main_job', hour = '7-19', minute = 3, timezone= 'America/New_York') #0 , jitter = 300)
 
-@scheduler.scheduled_job('cron', hour = '7-19', minute = 3, timezone= 'America/New_York')
+@scheduler.scheduled_job('cron', id="job_1", hour = '7-19', minute = 3, timezone= 'America/New_York')
 def chronological_job():
     cronjob()
 
