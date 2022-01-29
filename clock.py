f8b12f87d6af500895fd0d713fbbcabae731d767
meldesouki <43501322+meldesouki@users.noreply.github.com>
2022-01-29 12:06:08 -0500

    Updated clock.py

--- clock.py
+++ clock.py
@@ -2,6 +2,7 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 from occupancy_tracker import cronjob
 
 scheduler = BlockingScheduler()
-scheduler.add_job(cronjob, 'cron', day_of_week = '*', hour = '7-19')
+
+scheduler.add_job(cronjob(), 'cron', day_of_week = '*', hour = '7-19')
 
 scheduler.start()
